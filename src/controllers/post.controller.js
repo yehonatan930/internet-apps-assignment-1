f4e9d018fa2ae4e609ff030908ce187c48889536
johnny <324825470@idf.il>
2024-11-22 19:17:48 +0200

    updating post api

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -5,4 +5,19 @@ const router = express.Router();
 
 const Post = mongoose.model("Post", postSchema);
 
+router.put("/", async (req, res) => {
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(req.body.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedPost)
+      return res.status(404).json({ message: "Post not found" });
+    res.json(updatedPost);
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Error updating post", error: err.message });
+  }
+});
 module.exports = router;
