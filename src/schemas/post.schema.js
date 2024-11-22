1900afb07be54f9f8f03f070c97aec6af2e8a05d
johnny <324825470@idf.il>
2024-11-22 19:12:06 +0200

    added sender to schema

--- src/schemas/post.schema.js
+++ src/schemas/post.schema.js
@@ -1,7 +1,7 @@
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  sender: { type: String, required: true },
 });
 
 module.exports = { postSchema };
