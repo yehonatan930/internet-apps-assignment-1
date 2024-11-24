b6c5429702aa00a395280afd8bc77170e0fe7a6e
נועה <noa@>
2024-11-25 01:44:29 +0200

    add new comment request api

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,3 +5,12 @@ const router = express.Router();
 
 const Comment = mongoose.model('Comment', commentSchema);
 
+// Get All Comments
+app.get('/comments', async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.status(200).json(comments);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
