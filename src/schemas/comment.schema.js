f975b47886da7613e4a239760458dd0e4d370c22
johnny <324825470@idf.il>
2024-12-02 21:31:09 +0200

    fixed bugs

--- src/schemas/comment.schema.js
+++ src/schemas/comment.schema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const commentSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
   postId: { type: Number, ref: "Post", required: true },
   content: { type: String, required: true },
   author: { type: String, required: true },
