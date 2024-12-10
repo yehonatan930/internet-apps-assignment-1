ef80d5824aa6cd6432c79f68afd5c3f97708d0ad
johnny <324825470@idf.il>
2024-12-10 11:53:15 +0200

    add authenticate middeware and router

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access denied" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+export default authenticate;
