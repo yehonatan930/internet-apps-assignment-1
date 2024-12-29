391f7090f1852a35a86c14e33969658becee70fe
נועה <noa@>
2024-12-29 21:48:46 +0200

    change font

--- client/brooks/src/index.tsx
+++ client/brooks/src/index.tsx
@@ -8,6 +8,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const queryClient = new QueryClient();
 
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
