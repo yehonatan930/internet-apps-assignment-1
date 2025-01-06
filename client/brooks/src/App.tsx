8f1ff3486a2090380f71a187c47232e6ad1d23db
נועה <noa@>
2025-01-06 19:18:33 +0200

    add navbar

--- client/brooks/src/App.tsx
+++ client/brooks/src/App.tsx
@@ -4,36 +4,42 @@ import { Route, Routes } from 'react-router-dom';
 import RegistrationScreen from './components/RegistrationScreen/RegistrationScreen';
 import LoginScreen from './components/LoginScreen/LoginScreen';
 import ProfileScreen from './components/ProfileScreen/ProfileScreen';
-import { UserProvider } from './context/UserContext';
+import { UserProvider, useUser } from './context/UserContext';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
+function AppContent() {
+  const { user, setUser } = useUser();
+  const navigate = useNavigate();
+
+
   const handleError = (error: string) => {
     // Handle error logic here
     console.error('Error occurred:', error);
   };
 
   return (
+    <div className="App">
+      {user && <Navbar onLogout={handleLogout} />}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/login" element={<LoginScreen onError={handleError} />} />
+          <Route path="/register" element={<RegistrationScreen onError={handleError} />} />
+          <Route path="/profile" element={<ProfileScreen />} />
+          {/*<Route path="/" element={<HomeFeedScreen />} />*/}
+          {/*<Route path="/post/create" element={<PostCreationScreen />} />*/}
+          {/*<Route path="/discover" element={<DiscoverScreen />} />*/}
+          {/*<Route path="/post/:id" element={<PostDetailScreen />} />*/}
+          {/*<Route path="/profile/edit" element={<EditProfileScreen />} />*/}
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <UserProvider>
-      <div className="App">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            {/*<Route path="/" element={<HomeFeedScreen />} />*/}
-            <Route
-              path="/login"
-              element={<LoginScreen onError={handleError} />}
-            />
-            <Route
-              path="/register"
-              element={<RegistrationScreen onError={handleError} />}
-            />
-            {/*<Route path="/post/create" element={<PostCreationScreen />} />*/}
-            <Route path="/profile" element={<ProfileScreen />} />
-            {/*<Route path="/discover" element={<DiscoverScreen />} />*/}
-            {/*<Route path="/post/:id" element={<PostDetailScreen />} />*/}
-            {/*<Route path="/profile/edit" element={<EditProfileScreen />} />*/}
-          </Routes>
-        </Suspense>
-      </div>
+      <AppContent />
     </UserProvider>
   );
 }
