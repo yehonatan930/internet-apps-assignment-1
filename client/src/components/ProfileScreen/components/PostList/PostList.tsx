c854af5c52393cdd76f5996484bba9fe077804b9
נועה <noa@>
2025-01-12 16:56:19 +0200

    add message if there are no posts

--- client/src/components/ProfileScreen/components/PostList/PostList.tsx
+++ client/src/components/ProfileScreen/components/PostList/PostList.tsx
@@ -9,28 +9,31 @@ interface PostListProps {
 
 const PostList: React.FC<PostListProps> = ({ userId }) => {
   const { data: posts, isLoading: postsLoading } = useUserPosts();
-
   return (
     <div className="profile__posts">
       <h3>User Posts</h3>
       {postsLoading ? (
         <CircularProgress />
       ) : (
-        posts?.map((post) => (
-          <div key={post._id} className="profile__post">
-            {post.imageUrl && (
-              <img
-                src={post.imageUrl}
-                alt="Post"
-                className="profile__post-image"
-              />
-            )}
-            <div className="profile__post-content">
-              <h4>{post.bookTitle}</h4>
-              <p>{post.content}</p>
+        posts && posts.length > 0 ? (
+          posts.map((post) => (
+            <div key={post._id} className="profile__post">
+              {post.imageUrl && (
+                <img
+                  src={post.imageUrl}
+                  alt="Post"
+                  className="profile__post-image"
+                />
+              )}
+              <div className="profile__post-content">
+                <h4>{post.bookTitle}</h4>
+                <p>{post.content}</p>
+              </div>
             </div>
-          </div>
-        ))
+          ))
+        ) : (
+          <p>No posts available</p>
+        )
       )}
     </div>
   );
